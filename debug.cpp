8e4c2695e32ba1739ebae31c5699fd26f25620b8
rafiul41 <rafiul1994@gmail.com>
2024-06-05 13:43:53 +0600

    Updated task for c++ in mac

--- debug.cpp
+++ debug.cpp
@@ -1,54 +1,11 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 
 using namespace std;
 
-int leastInterval(vector<char>& tasks, int n) {
-  vector<int> cnt (26, 0);
-  for(int i = 0; i < tasks.size(); i++) {
-    cnt[tasks[i] - 'A']++;
-  }
-
-  priority_queue<int> pq;
-  for(int i = 0; i < 26; i++) {
-    if(cnt[i] != 0) {
-      pq.push(cnt[i]);
-    }
-  }
-
-
-  int taken = 0, ans = 0;
-  while(true) {
-    vector<int> tmp;
-    int currTaken = 0;
-    for(int i = 0; i < n + 1; i++) {
-      if(pq.empty()) {
-        break;
-      }
-      int top = pq.top();
-      pq.pop();
-      tmp.push_back(top);
-      currTaken++;
-    }
-    taken += currTaken;
-    for(int i = 0; i < tmp.size(); i++) {
-      if(tmp[i] - 1 > 0) {
-        pq.push(tmp[i] - 1);
-      }
-    }
-
-    if(taken == tasks.size()) {
-      ans += currTaken;
-      break;
-    } else {
-      ans += (n + 1);
-    }
-  }
-
-  return ans;
-}
-
 int main() {
-  vector<char> s = {'A','A','A','A','A','A','B','C','D','E','F','G'};
-  cout<<leastInterval(s, 2);
+  int a = 23;
+  int b = 44;
+  int c = a + b;
+  cout<<"result is : "<<c<<endl;
   return 0;
 }
--- debug.js
+++ debug.js
@@ -1,5 +1,6 @@
 let map = new Map();
 map.set('a', 2);
+map.set('b', 3);
 
 for(let [a, b] of map) {
   console.log(a, b);
