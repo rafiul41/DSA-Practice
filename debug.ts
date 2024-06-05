d2f021efbd05e14fade44749a75e4d9800870dfd
rafiul41 <rafiul1994@gmail.com>
2024-06-05 13:52:45 +0600

    Typescript and Javascript configs

--- debug.ts
+++ debug.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+let map = new Map();
+map.set('a', 2);
+map.set('b', 3);
+
+let arr: string[] = ['a', 'b', 'c'];
+
+for(let [a, b] of map) {
+  console.log(a, b);
+}
+
+for(let char of arr) {
+  console.log(char);
+}
