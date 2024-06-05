67306f7c39fb55f9db51d2022cd31b26ad576102
rafiul41 <rafiul1994@gmail.com>
2024-06-05 18:16:48 +0600

    Added an element

--- debug.ts
+++ debug.ts
@@ -2,7 +2,7 @@ let map = new Map();
 map.set('a', 2);
 map.set('b', 3);
 
-let arr: string[] = ['a', 'b', 'c'];
+let arr: string[] = ['a', 'b', 'c', 'd'];
 
 for(let [a, b] of map) {
   console.log(a, b);
