5c286170ed1db23fc3c6cd7c1b8132eb868a08c4
rafiul41 <rafiul1994@gmail.com>
2024-06-11 17:12:52 +0600

    tasks json edit for c++ debug

--- debug.cpp
+++ debug.cpp
@@ -1,11 +1,25 @@
 #include "bits/stdc++.h"
 
-using namespace std;
+vector<int> solve(vector<int> &A) {
+    int p1 = 0, p2 = A.size() - 1;
+    while(p1 < p2) {
+      if(A[p1] == 0) {
+        p1++;
+      } else {
+        swap(A[p1], A[p2]);
+        p2--;
+      }
+    }
+    return A;
+}
 
 int main() {
-  int a = 23;
-  int b = 44;
-  int c = a + b;
-  cout<<"result is : "<<c<<endl;
+  vector<int> A {1, 0, 0};
+  vector<int> res = solve(A);
+  for(int i = 0; i < A.size(); i++) {
+    cout<<A[i]<<" ";
+  }
+  cout<<endl;
+
   return 0;
 }
